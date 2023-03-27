8a2172e92d730a2f660e06d1989106eec3d5255f
Rafatulislam60 <rafatulislam67@gmail.com>
2023-03-28 00:09:15 +0600

    added cart

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
 import Header from "./components/Header/Header";
-import Shop from "./components/Shop/shop";
+import Shop from "./components/Shop/Shop";
 
 function App() {
   const [count, setCount] = useState(0);
